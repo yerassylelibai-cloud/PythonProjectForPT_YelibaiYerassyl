862fda8fe47b212473b2650638053e4e0d980fa0
erasil <erasilelibai@mail.ru>
2026-05-06 23:21:43 +0500

    2

--- main.py
+++ main.py
@@ -10,47 +10,47 @@ from fastapi.responses import FileResponse
 from fastapi import FastAPI, UploadFile, File
 import io
 
-def main(data):
-    print(FindMAE(data).CalculateMAE())
-    print(Table(data).BuildTable())
-
-    Graph(data).BuildGraph()
-
-
-
-
-main(np.genfromtxt('externals/body.csv', delimiter=',', skip_header=1))
+# def main(data):
+#     print(FindMAE(data).CalculateMAE())
+#     print(Table(data).BuildTable())
+#
+#     Graph(data).BuildGraph()
+#
+#
+#
+#
+# main(np.genfromtxt('externals/body.csv', delimiter=',', skip_header=1))
 
 
 
 app = FastAPI()
 
 
-# @app.post("/analyze")
-# async def analyze_data(file: UploadFile = File(...)):
-#     # 1. Read the uploaded file into a NumPy array
-#     content = await file.read()
-#     raw_data = np.genfromtxt(io.BytesIO(content), delimiter=',', skip_header=1)
-#
-#
-#
-#     mae_calculator = FindMAE(raw_data)
-#     mae_value = mae_calculator.CalculateMAE()
-#
-#     Graph(raw_data).BuildGraph()
-#
-#     # 3. Return the JSON (This is the "Output" for Task 14)
-#     return {
-#         "coefficients": {
-#             "slope_a": float(mae_calculator.a),
-#             "intercept_b": float(mae_calculator.b)
-#         },
-#         'metrics': mae_value,
-#         'table': Table(raw_data).BuildTable().to_dict(),
-#         "status": "Success"
-#
-#     }
-#
-# @app.get("/view-graph")
-# async def view_graph():
-#     return FileResponse("result.png")
+@app.post("/analyze")
+async def analyze_data(file: UploadFile = File(...)):
+    # 1. Read the uploaded file into a NumPy array
+    content = await file.read()
+    raw_data = np.genfromtxt(io.BytesIO(content), delimiter=',', skip_header=1)
+
+
+
+    mae_calculator = FindMAE(raw_data)
+    mae_value = mae_calculator.CalculateMAE()
+
+    Graph(raw_data).BuildGraph()
+
+    # 3. Return the JSON (This is the "Output" for Task 14)
+    return {
+        "coefficients": {
+            "slope_a": float(mae_calculator.a),
+            "intercept_b": float(mae_calculator.b)
+        },
+        'metrics': mae_value,
+        'table': Table(raw_data).BuildTable().to_dict(),
+        "status": "Success"
+
+    }
+
+@app.get("/view-graph")
+async def view_graph():
+    return FileResponse("result.png")
